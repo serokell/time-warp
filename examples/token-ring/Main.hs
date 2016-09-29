a07c1f985c7acdeb9f39455fa60e1a0c84c1d1c4
Martoon <Martoon_00@mail.ru>
2016-09-29 11:35:43 +0300

    [TW-17] Make paramaters global values

--- examples/token-ring/Main.hs
+++ examples/token-ring/Main.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE FlexibleInstances    #-}
 {-# LANGUAGE TupleSections        #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE ConstraintKinds      #-}
 
 import           Control.Exception          (Exception)
-import           Control.Monad              (forever, when, forM_)
+import           Control.Monad              (forever, when, forM_, unless, join)
 import           Control.Monad.Catch        (MonadCatch, catch)
 import           Control.Monad.Random       (StdGen, mkStdGen)
+import qualified Control.Concurrent.STM.TVar as T
+import           Control.Concurrent.STM     (atomically)
 import           Control.Monad.Trans        (MonadIO (..), lift)
 import           Control.Lens               (Iso', iso, (^.))
 import           Formatting                 (sformat, shown, (%))
@@ -16,36 +19,56 @@ import           Control.TimeWarp.Logging   (WithNamedLogger (..), usingLoggerNa
                                              logInfo, initLogging, setLoggerName,
                                              Severity (..),
                                              LoggerName (..),
-                                             logDebug, LoggerNameBox)
+                                             logDebug, logError, LoggerNameBox)
 import           Control.TimeWarp.Timed     (MonadTimed (..), schedule, ThreadId,
                                              killThread, at, for, sleepForever,
-                                             Microsecond, sec,
-                                             interval, invoke, after)
+                                             Microsecond, sec, ms,
+                                             interval, invoke, after, fork_)
 import           Control.TimeWarp.Rpc       (MonadRpc (..), Port, NetworkAddress,
                                              Client, ServerT, call, method, runPureRpc,
                                              runMsgPackRpc, DelaysSpecifier,
                                              serverTypeRestriction1, MsgPackRpc,
-                                             PureRpc)
+                                             PureRpc, getRandomTR, Delays (..),
+                                             ConnectionOutcome (..))
+
+-- * Launch parameters.
+
+launchDuration :: Microsecond
+launchDuration = interval 200 sec
+
+tokenPassingDelay :: Microsecond
+tokenPassingDelay = interval 3 sec
+
+nodeNumber :: Int
+nodeNumber = 3
+
+observerCheckInterval :: Microsecond
+observerCheckInterval = interval 3 sec
+
+networkDelay :: (Microsecond, Microsecond)
+networkDelay = (interval 1 ms, interval 5 ms)
+
+-- * Starter
 
 main :: IO ()
 main = do
-    initLogging ["node"] Info
-    let Context {..} = context
+    initLogging ["node", "observer"] Info
 --    runRealMode $
-    runEmulationMode (0 :: Microsecond) (mkStdGen 0) $
+    runEmulationMode delays (mkStdGen 0) $ do
         setLoggerName "node" $ do
-            forM_ [1 .. _nodeNum] $
+            forM_ [1 .. nodeNumber] $
                 \no -> modifyLoggerName (<> LoggerName (show no)) $
-                    fork $ launchNode context no
-     
+                    fork $ launchNode no
+
+        setLoggerName ("observer" <> "progress") $
+            fork_ $
+                launchObserver
   where
-    context =
-        Context
-            { _duration = interval 20 sec
-            , _tokenDelay = interval 3 sec
-            , _nodeNum = 3
-            , _gotToken = return ()
-            }
+    delays = Delays $
+                \dest _ -> 
+                    if dest == (observerPort ^. fullAddr)
+                    then return $ ConnectedIn 0
+                    else ConnectedIn <$> getRandomTR networkDelay
 
 runRealMode :: LoggerNameBox MsgPackRpc () -> IO ()
 runRealMode = runMsgPackRpc . usingLoggerName mempty
@@ -61,27 +84,19 @@ nodePort = iso (+2000) (subtract 2000)
 fullAddr :: Iso' Port NetworkAddress
 fullAddr = iso ("127.0.0.1", ) snd
 
-class ( MonadTimed m
+type WorkMode m
+    = ( MonadTimed m
       , MonadRpc m
       , WithNamedLogger m
       , MonadIO m
-      , MonadCatch m) => WorkMode m
+      , MonadCatch m)
 
-instance ( MonadTimed m
-         , MonadRpc m
-         , WithNamedLogger m
-         , MonadIO m
-         , MonadCatch m) => WorkMode m
+-- * Nodes
 
-data Context = Context
-    { _duration   :: Microsecond
-    , _tokenDelay :: Microsecond
-    , _nodeNum    :: Int
-    , _gotToken   :: IO ()
-    }
+type TokenValue = Int
 
-launchNode :: WorkMode m => Context -> Int -> m ()
-launchNode Context{..} no = do
+launchNode :: WorkMode m => Int -> m ()
+launchNode no = do
     logDebug $ sformat ("Launching node " % shown) no
 
     -- worker thread, which waits for signal from server thread
@@ -96,13 +111,13 @@ launchNode Context{..} no = do
         fork $ do
             idr <- serverTypeRestriction1
             logDebug $ sformat ("Server up at " % shown) $ no ^. nodePort
-            serve (no ^. nodePort) 
+            serve (no ^. nodePort)
                 [ method "token" $ idr $ acceptToken wtid
                 ]
 
     -- kill all when testing finishes
     modifyLoggerName (<> "killer") $
-        schedule (at _duration) $ do
+        schedule (at launchDuration) $ do
             mapM_ killThread [wtid, stid]
 
     logInfo "Launched"
@@ -115,23 +130,76 @@ launchNode Context{..} no = do
   where
     onValueReceived (ValueReceived v) = do
         logInfo $ sformat ("Got token with value " % shown) v
-        wait (for _tokenDelay)
+        execClient ("127.0.0.1", 5001) $ noteTokenCall v
+        wait (for tokenPassingDelay)
         initPassingToken $ v + 1
-    
+
     initPassingToken v = do
-        let targetNode = no `mod` _nodeNum + 1
+        let targetNode = no `mod` nodeNumber + 1
             targetAddr = targetNode ^. nodePort . fullAddr
         logDebug "Passing token"
         execClient targetAddr $ passToken v
 
-    passToken :: Int -> Client ()
+    passToken :: TokenValue -> Client ()
     passToken = call "token"
 
-    acceptToken :: WorkMode m => ThreadId m -> Int -> ServerT m ()
+    acceptToken :: WorkMode m => ThreadId m -> TokenValue -> ServerT m ()
     acceptToken tid value = do
-        lift $ throwTo tid $ ValueReceived value
+        lift . throwTo tid $ ValueReceived value
 
 data SignalException = ValueReceived Int
     deriving (Show, Typeable)
 
 instance Exception SignalException
+
+-- * Observer, checks for global predicates.
+
+observerPort :: Int
+observerPort = 5000
+
+launchObserver :: WorkMode m => m ()
+launchObserver = do
+    lastProgress <- liftIO $ T.newTVarIO (0, 0)
+
+    -- server which listens whether token value was changed
+    stid <- modifyLoggerName (<> "server") $
+        fork $ do
+            idr <- serverTypeRestriction1
+            serve observerPort
+                [method "noteToken" $ idr $ noteTokenMethod lastProgress
+                ]
+
+    -- periodically checks for progress
+    ctid <- modifyLoggerName (<> "checker") $
+        fork . forever $ do
+            wait (for 1 sec)
+            (lastTime, value) <- liftIO $ T.readTVarIO lastProgress
+            time <- virtualTime
+            when (time - lastTime > observerCheckInterval) $
+                logError $
+                    sformat ("Token value (" % shown % ") hasn't changed " %
+                             "since " % shown) value lastTime
+
+    logInfo "Launched"
+
+    -- kill all when testing finishes
+    modifyLoggerName (<> "killer") $
+        schedule (at launchDuration) $ do
+            mapM_ killThread [stid, ctid]
+
+
+noteTokenMethod
+    :: WorkMode m
+    => T.TVar (Microsecond, TokenValue) -> TokenValue -> ServerT m ()
+noteTokenMethod progressBox value = lift $ do
+    time <- virtualTime
+    join . liftIO . atomically $ do
+        (_, wasValue) <- T.readTVar progressBox
+        T.writeTVar progressBox (time, value)
+        return $
+            unless (value == wasValue + 1) $
+                logError $ sformat ("Wrong token value: expected " % shown %
+                                    " but got " % shown) (wasValue + 1) value
+
+noteTokenCall :: TokenValue -> Client ()
+noteTokenCall = call "noteToken"
