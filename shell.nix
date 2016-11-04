bd82e38cd769166474fad708ccde55779f7e3226
Mikhail Volkhov <volhovm.cs@gmail.com>
2016-11-04 09:39:01 +0300

    Update to lts 7.5

--- shell.nix
+++ shell.nix
@@ -1,9 +1,6 @@
-let 
-  lts = "lts-6_7";
-in 
-  with import <nixpkgs> { };
-  haskell.lib.buildStackProject {
-     ghc = haskell.packages.${lts}.ghc;
-     name = "rscoin-core";
-     buildInputs = [ zlib glib git cabal-install openssh autoreconfHook stack ];
-  }
+with import <nixpkgs> { };
+haskell.lib.buildStackProject {
+   ghc = haskell.packages.ghc801.ghc;
+   name = "rscoin-core";
+   buildInputs = [ zlib glib git cabal-install openssh autoreconfHook stack ];
+}
