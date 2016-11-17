c31374c9317a9774a11aba0119375cde0405205d
Martoon <Martoon_00@mail.ru>
2016-11-18 00:50:08 +0300

    [TW-38] Print estimate in launch.hs

--- bench/launch.sh
+++ bench/launch.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+echo "Wait for 10 sec while gathering results..."
 (stack exec bench-receiver &> /dev/null) & (sleep 1; stack exec bench-sender &> /dev/null)
 stack exec bench-log-reader
