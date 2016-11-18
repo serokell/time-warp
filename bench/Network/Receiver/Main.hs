f8738ac46789ae0874e105c7a8312586ec871871
Martoon <Martoon_00@mail.ru>
2016-11-18 17:39:15 +0300

    [TW-38] Make Pong optional

--- bench/Network/Receiver/Main.hs
+++ bench/Network/Receiver/Main.hs
@@ -1,3 +1,4 @@
+import           Control.Monad            (when)
 import           Control.Monad.Trans      (liftIO)
 import           GHC.IO.Encoding          (setLocaleEncoding, utf8)
 
@@ -16,12 +17,15 @@ main = runNode "receiver" $ do
         initLoggingFromYaml "logging.yaml"
     liftIO $ setLocaleEncoding utf8
 
+    let pongEnabled = True
+
     stopper <- listen (AtPort 3456)
         [ Listener $
             \(Ping mid) -> do
                 logMeasure PingReceived mid
-                logMeasure PongSent mid
-                reply $ Pong mid
+                when pongEnabled $ do
+                    logMeasure PongSent mid
+                    reply $ Pong mid
         ]
     wait (for 10 sec)
     stopper
