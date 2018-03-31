a64b8448443ea78311c38f9897961be80069c690
Martoon <martoon.hsk@gmail.com>
2018-03-31 14:00:05 +0300

    Add addresses resolution caching to MsgPackUdp
    Also added options where caching can be disabled, and where message size limit
    may be changed.

--- src/Control/TimeWarp/Rpc/MsgPackUdp.hs
+++ src/Control/TimeWarp/Rpc/MsgPackUdp.hs
@@ -15,29 +15,34 @@
 module Control.TimeWarp.Rpc.MsgPackUdp
     ( MsgPackUdp (..)
     , runMsgPackUdp
+    , runMsgPackUdpOpts
     ) where
 
 import           Control.Lens                  ((<&>))
 import           Control.Monad                 (forever, when)
 import           Control.Monad.Base            (MonadBase)
 import           Control.Monad.Catch           (Exception, MonadCatch, MonadMask,
-                                                MonadThrow, bracket, onException, throwM)
-import           Control.Monad.Reader          (ReaderT (..))
+                                                MonadThrow, bracket, throwM)
+import           Control.Monad.Reader          (ReaderT (..), ask)
 import           Control.Monad.Trans           (MonadIO (..))
 import           Control.Monad.Trans.Control   (MonadBaseControl (..))
-import           Control.TimeWarp.Rpc.MonadRpc (Method (..), MonadRpc (..),
+import           Control.TimeWarp.Rpc.MonadRpc (Host, Method (..), MonadRpc (..),
                                                 NetworkAddress, RpcOptionMessagePack,
                                                 RpcOptionNoReturn, messageId,
                                                 methodMessageId)
 import           Control.TimeWarp.Timed        (MonadTimed, ThreadId, TimedIO, runTimedIO)
 import qualified Data.Binary                   as Binary
 import qualified Data.ByteString.Lazy          as LBS
+import           Data.Default                  (Default (..))
+import qualified Data.HashMap.Strict           as HM
+import qualified Data.IORef.Lifted             as Ref
 import qualified Data.Map                      as M
 import           Data.Maybe                    (fromMaybe)
 import qualified Data.MessagePack              as MsgPack
 import           Data.Proxy                    (Proxy (..))
 import qualified Data.Text                     as T
 import           Formatting                    (sformat, shown, (%))
+import           GHC.IO.Unsafe                 (unsafePerformIO)
 import qualified Network.Socket                as N hiding (recv, sendTo)
 import qualified Network.Socket.ByteString     as N
 import           Text.Read                     (readMaybe)
@@ -50,15 +55,37 @@ withUdpSocket = bracket mkSocket close
     mkSocket = liftIO $ N.socket N.AF_INET N.Datagram N.defaultProtocol
     close = liftIO . N.close
 
+newtype CacheAddrs = CacheAddrs Bool
+
+data MsgPackUdpOptions = MsgPackUdpOptions
+    { udpCacheAddresses   :: CacheAddrs
+      -- ^ Globally cache resolved addresses.
+      -- This may give almost x2 speed up on small packets.
+    , udpMessageSizeLimit :: Int
+      -- ^ Limit on sent/received packet size.
+    }
+
+instance Default MsgPackUdpOptions where
+    def =
+        MsgPackUdpOptions
+        { udpCacheAddresses = CacheAddrs True
+        , udpMessageSizeLimit = 1500
+        }
+
+type LocalEnv = (MsgPackUdpOptions, N.Socket)
+
 newtype MsgPackUdp a = MsgPackUdp
-    { unwrapMsgPackRpc :: ReaderT N.Socket TimedIO a
+    { unwrapMsgPackRpc :: ReaderT LocalEnv TimedIO a
     } deriving (Functor, Applicative, Monad, MonadIO, MonadBase IO,
                 MonadThrow, MonadCatch, MonadMask, MonadTimed)
 
-runMsgPackUdp :: MsgPackUdp a -> IO a
-runMsgPackUdp (MsgPackUdp action) = do
+runMsgPackUdpOpts :: MsgPackUdpOptions -> MsgPackUdp a -> IO a
+runMsgPackUdpOpts options (MsgPackUdp action) = do
     withUdpSocket $ \sock ->
-        runTimedIO $ runReaderT action sock
+        runTimedIO $ runReaderT action (options, sock)
+
+runMsgPackUdp :: MsgPackUdp a -> IO a
+runMsgPackUdp = runMsgPackUdpOpts def
 
 type instance ThreadId MsgPackUdp = ThreadId TimedIO
 
@@ -80,27 +107,30 @@ newtype DecodeException = DecodeException T.Text
 instance Exception DecodeException
 
 instance MonadRpc LocalOptions MsgPackUdp where
-    send addr (msg :: msg) = MsgPackUdp . ReaderT $ \sock -> do
+    send addr (msg :: msg) = MsgPackUdp . ReaderT $
+      \(MsgPackUdpOptions{..}, sock) -> do
         let msgName = messageId @msg Proxy
             rawMsg = (msgName, msg)
             dat = Binary.encode $ MsgPack.toObject rawMsg
 
         let msgSize = fromIntegral $ LBS.length dat
-        when (msgSize > fromIntegral maxMsgSize) $
+        when (msgSize > fromIntegral udpMessageSizeLimit) $
             throwM $ PacketSizeOverflow msgSize
 
-        liftIO $ N.sendManyTo sock (LBS.toChunks dat) (networkAddrToSockAddr addr)
+        sockAddr <- networkAddrToSockAddr udpCacheAddresses addr
+        liftIO $ N.sendManyTo sock (LBS.toChunks dat) sockAddr
 
     serve (fromIntegral -> port) methods = do
         -- one socket per listener
         withUdpSocket $ \sock -> do
-            liftIO $ N.bind sock (N.SockAddrInet port N.iNADDR_ANY) `onException` putStrLn "bind failed"
+            liftIO $ N.bind sock (N.SockAddrInet port N.iNADDR_ANY)
             forever $ receive sock
       where
         methodsMap = M.fromList $ methods <&> \m -> (methodMessageId m, m)
         receive :: N.Socket -> MsgPackUdp ()
         receive sock = do
-            dat <- liftIO $ N.recv sock maxMsgSize
+            (MsgPackUdpOptions{..}, _) <- MsgPackUdp ask
+            dat <- liftIO $ N.recv sock udpMessageSizeLimit
 
             either (throwM . DecodeException) id $ do
                 obj <- case Binary.decodeOrFail (LBS.fromStrict dat) of
@@ -121,17 +151,31 @@ instance MonadRpc LocalOptions MsgPackUdp where
 
                 return action
 
-maxMsgSize :: Int
-maxMsgSize = 1500
+addrCache :: Ref.IORef (HM.HashMap Host N.HostAddress)
+addrCache = unsafePerformIO $ Ref.newIORef mempty
 
-networkAddrToSockAddr :: NetworkAddress -> N.SockAddr
-networkAddrToSockAddr (ip, port) =
-    let ip' =
+takeInAddrCache :: MonadBase IO m => (Host, N.HostAddress) -> m N.HostAddress
+takeInAddrCache (host, candidateAddr) = do
+    maddr <- HM.lookup host <$> Ref.readIORef addrCache
+    case maddr of
+        Nothing -> do
+            Ref.atomicModifyIORef addrCache ((, ()) . HM.insert host candidateAddr)
+            return candidateAddr
+        Just addr -> return addr
+
+networkAddrToSockAddr :: MonadBase IO m => CacheAddrs -> NetworkAddress -> m N.SockAddr
+networkAddrToSockAddr (CacheAddrs useCaching) (ip, port) = do
+    let hostAddr' =
           case map parse (split ip) of
             [a1, a2, a3, a4] -> N.tupleToHostAddress (a1, a2, a3, a4)
             _                -> incorrectIp
-    in  N.SockAddrInet (fromIntegral port) ip'
+    hostAddr <-
+        if useCaching
+        then takeInAddrCache (ip, hostAddr')
+        else pure hostAddr'
+    return $ N.SockAddrInet (fromIntegral port) hostAddr
   where
     incorrectIp = error "send: incorrect api"
     split = T.splitOn "."
     parse = fromMaybe incorrectIp . readMaybe . T.unpack
+
